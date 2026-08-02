c6f0faf8c36cb4ba143676d9536d8d6fefe6d92a
Ayas Ibrahim <aayasabraham@gmail.com>
2026-08-03 00:13:41 +0600

    feat: implement patient management API; add routes, controller, service, and interface for patient operations

--- src/modules/patient/patient.controller.ts
+++ src/modules/patient/patient.controller.ts
@@ -1,11 +1,70 @@
-// const getDoctorPatients = catchAsync(async (req: Request, res: Response) => {});
-// const addPatientUnderDoctor = catchAsync(
-//   async (req: Request, res: Response) => {
-//     const { id: doctorId } = req.params;
-//     const patientData = req.body;
-//     const result = await patientService.addPatientUnderDoctor(
-//       doctorId,
-//       patientData,
-//     );
-//   },
-// );
+import httpStatus from "http-status";
+import { Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
+import { patientService } from "./patient.service";
+
+const getAllPatients = catchAsync(async (req: Request, res: Response) => {
+  const q = req.query;
+  const result = await patientService.getAllPatientsFromDB(q);
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Patients retrieved  Successfully!",
+    data: result.data,
+    meta: result.meta,
+  });
+});
+const getSinglePatient = catchAsync(async (req: Request, res: Response) => {
+  const { id: patientId } = req.params;
+  if (!patientId) {
+    throw new Error("Patient Id Required In Params");
+  }
+  const result = await patientService.getSinglePatientFromDB(
+    patientId as string,
+  );
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Patient retrieved  Successfully!",
+    data: result,
+  });
+});
+const updatePatient = catchAsync(async (req: Request, res: Response) => {
+  const payload = req.body;
+  const { id: patientId } = req.params;
+  if (!patientId) {
+    throw new Error("Patient Id Required In Params");
+  }
+  const result = await patientService.updatePatientIntoDB(
+    patientId as string,
+    payload,
+  );
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Patient Upddate  Successfully",
+    data: result,
+  });
+});
+
+const deletePatient = catchAsync(async (req: Request, res: Response) => {
+  const { id: patientId } = req.params;
+  if (!patientId) {
+    throw new Error("Doctor Id Required In Params");
+  }
+  await patientService.deletePatientIntoDB(patientId as string);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Patient Delete  Successfully",
+    data: null,
+  });
+});
+export const patientController = {
+  getAllPatients,
+  getSinglePatient,
+  updatePatient,
+  deletePatient,
+};
---
+++ src/modules/patient/patient.service.ts
@@ -0,0 +1,102 @@
+import { IPatient, IPatientQuery } from "./patient.interface";
+import Patient from "./patient.model";
+
+const getAllPatientsFromDB = async (q: IPatientQuery) => {
+  const limit = q.limit ? Number(q.limit) : 10;
+  const page = q.page ? Number(q.page) : 1;
+  const skip = (page - 1) * limit;
+
+  const sortBy = q.sortBy ? q.sortBy : "createdAt";
+  const sortOrder = q.sortOrder === "asc" ? 1 : -1;
+
+  const andConditions: any[] = [];
+
+  //  Search (Name, Phone, Condition)
+  if (q.searchTerm) {
+    andConditions.push({
+      $or: [
+        { name: { $regex: q.searchTerm, $options: "i" } },
+        { phone: { $regex: q.searchTerm, $options: "i" } },
+        { condition: { $regex: q.searchTerm, $options: "i" } },
+      ],
+    });
+  }
+
+  //  Exact Filters (Gender, DoctorId)
+  if (q.gender) {
+    andConditions.push({ gender: q.gender });
+  }
+
+  if (q.doctorId) {
+    andConditions.push({ doctorId: q.doctorId });
+  }
+
+  const whereConditions =
+    andConditions.length > 0 ? ({ $and: andConditions } as any) : {};
+
+  const patients = await Patient.find(whereConditions)
+    .populate({
+      path: "doctorId",
+      select: "name specialization hospital email phone",
+    })
+    .sort({ [sortBy]: sortOrder })
+    .skip(skip)
+    .limit(limit)
+    .select("-__v");
+
+  const totalPatientsCount = await Patient.countDocuments(whereConditions);
+
+  return {
+    data: patients,
+    meta: {
+      page,
+      limit,
+      total: totalPatientsCount,
+      totalPages: Math.ceil(totalPatientsCount / limit),
+    },
+  };
+};
+const getSinglePatientFromDB = async (patientId: string) => {
+  const patient = await Patient.findOne({ _id: patientId } as any);
+  if (!patient) {
+    throw new Error("Patient not found");
+  }
+  return patient;
+};
+const updatePatientIntoDB = async (
+  patientId: string,
+  payload: Partial<IPatient>,
+) => {
+  const isExistPatient = await Patient.findOne({ _id: patientId } as any);
+  if (!isExistPatient) {
+    throw new Error("Patient not found");
+  }
+  const updatedPatient = await Patient.findOneAndUpdate(
+    { _id: patientId } as any,
+    { $set: payload },
+    {
+      new: true,
+      runValidators: true,
+    } as any,
+  ).select("-__v");
+
+  return updatedPatient;
+};
+
+const deletePatientIntoDB = async (patientId: string) => {
+  const isPatientExist = await Patient.findOne({ _id: patientId } as any);
+
+  if (!isPatientExist) {
+    throw new Error(" Doctor Not found! ");
+  }
+  const patientDoctor = await Patient.findOneAndDelete({ _id: patientId} as any);
+  return patientDoctor;
+};
+
+
+export const patientService = {
+  getAllPatientsFromDB,
+  getSinglePatientFromDB,
+  updatePatientIntoDB,
+  deletePatientIntoDB,
+};
